02ad4a1952b64b0f6be0dea3cfe9cd06365d246c
jam0928 <jack020928@naver.com>
2024-11-01 13:00:30 +0900

    Feat: 가게에 리뷰 추가

---
+++ src/services/review.service.js
@@ -0,0 +1,15 @@
+import { createReview, checkRestaurantExists } from '../repositories/review.repository.js';
+
+export const addReview = async (data) => {
+  const exists = await checkRestaurantExists(data.restaurantId);
+  if (!exists) {
+    throw new Error("리뷰를 추가하려는 가게가 존재하지 않습니다.");
+  }
+  const reviewId = await createReview({
+    restaurantId: data.restaurantId,
+    userId: data.userId,
+    rate: data.rate,
+    content: data.content
+  });
+  
+};
