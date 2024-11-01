65224bb4759f2d10a04834b1a73a56fa23e4724e
jam0928 <jack020928@naver.com>
2024-11-01 12:51:45 +0900

    Feat: 가게 추가 기능

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { handleUserSignUp } from "./controllers/user.controller.js";
+import { handleAddRestaurant } from "./controllers/restaurant.controller.js"; 
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/v1/users/signup", handleUserSignUp);
+app.post("/api/v1/restaurants", handleAddRestaurant); 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
