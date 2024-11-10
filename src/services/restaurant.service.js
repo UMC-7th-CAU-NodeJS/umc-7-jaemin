9201245765be084bcaff01793c9560e51c64a640
jam0928 <jack020928@naver.com>
2024-11-10 15:00:02 +0900

    Feat: 에러 처리 일부 추가2
    - 식당별 리뷰 조회할 때 식당이 존재하지 않는 경우
    - 식당별 미션 조회할 때 식당이 존재하지 않는 경우

--- src/services/restaurant.service.js
+++ src/services/restaurant.service.js
@@ -2,6 +2,8 @@ import { createRestaurant } from '../repositories/restaurant.repository.js';
 import { getAllStoreReviews, getAllStoreMissions } from '../repositories/user.repository.js';
 import { responseFromReviews, responseFromMissions, responseFromRestaurant } from '../dtos/restaurant.dto.js';
 import { getRestaurant } from '../repositories/restaurant.repository.js';
+import { checkRestaurantExists } from '../repositories/review.repository.js';
+import { NoRestaurant } from '../errors.js';
 
 export const addRestaurant = async (data) => {
     const restaurantId = await createRestaurant({
@@ -15,11 +17,19 @@ export const addRestaurant = async (data) => {
   }
 
 export const listStoreReviews = async (restaurantId, cursor = 0) => {
+  const exists = await checkRestaurantExists(restaurantId);
+  if (!exists) {
+    throw new NoRestaurant("리뷰를 조회하려는 가게가 존재하지 않습니다.", restaurantId);
+  }
   const reviews = await getAllStoreReviews(restaurantId,cursor);
   return responseFromReviews(reviews);
 };
 
 export const listStoreMissions = async (restaurantId, cursor = 0) => {
+  const exists = await checkRestaurantExists(restaurantId);
+  if (!exists) {
+    throw new NoRestaurant("미션을 조회하려는 가게가 존재하지 않습니다.", restaurantId);
+  }
   const missions = await getAllStoreMissions(restaurantId,cursor);
   return responseFromMissions(missions);
 };
