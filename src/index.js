dfe2d127ab531add7083d38538766f0905e4d612
jam0928 <jack020928@naver.com>
2024-11-16 09:51:03 +0900

    Feat: Swagger 세팅
    Swagger 기초 설정 및 8주차 워크북 실습

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 import { handleUserSignUp } from "./controllers/user.controller.js";
 import { handleAddRestaurant } from "./controllers/restaurant.controller.js"; 
 import { handleAddReview } from "./controllers/review.controller.js";
@@ -15,6 +17,38 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+//Swagger 설정
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = ["./src/index.js"];
+  const doc = {
+    info: {
+      title: "UMC 7th",
+      description: "UMC 7th Node.js 테스트 프로젝트입니다.",
+    },
+    host: "localhost:3000",
+  };
+
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
 
 /**
  * 공통 응답을 사용할 수 있는 헬퍼 함수 등록
