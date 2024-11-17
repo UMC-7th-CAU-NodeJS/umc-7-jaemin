84e358687bc635d5efb6a47e68f34d2516919ac8
jam0928 <jack020928@naver.com>
2024-11-17 18:02:25 +0900

    Fix: addRestaurnt 오류 수정
    checkRestaurantExists이 id만으로 식당의 존재 여부를 파악하기에, addRestaurant에서 쓰기엔 부적합했었다. 그래서 처음에 식당의 모든 속성을 비교하는 것으로 수정했었는데 이는 checkRestaurantExists를 사용하는 다른 API들에게 부적합했다. 따라서 checkRestaurantExistsForAddRestaurant을 따로 만들어서 이를 해결하였다.

--- src/services/restaurant.service.js
+++ src/services/restaurant.service.js
@@ -1,19 +1,20 @@
 import { createRestaurant } from '../repositories/restaurant.repository.js';
 import { responseFromRestaurant } from '../dtos/restaurant.dto.js';
-import { getRestaurant, checkRestaurantExists } from '../repositories/restaurant.repository.js';
+import { getRestaurant, checkRestaurantExists, checkRestaurantExistForAddRestaurant } from '../repositories/restaurant.repository.js';
 import { ResataurantAlreadyExistError } from '../errors.js';
 
 export const addRestaurant = async (data) => {
+    const exists = await checkRestaurantExistForAddRestaurant(data);
+    if (exists) {
+      throw new ResataurantAlreadyExistError("식당이 이미 존재합니다.", data);
+    }
     const restaurantId = await createRestaurant({ // restaurantId가 아니라 restaurantData로 수정해야 할 듯
       name: data.name,
       type: data.type,
       address: data.address,
       currentRegion: data.currentRegion
     });
-    const exists = await checkRestaurantExists(restaurantId);
-    if (exists) {
-      throw new ResataurantAlreadyExistError("식당이 이미 존재합니다.", restaurantId);
-    }
+    
     const restaurant = await getRestaurant(restaurantId);
     return responseFromRestaurant(restaurant)
   }
