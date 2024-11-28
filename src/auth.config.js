b63a33bf83a2e1de67de01ce60cc0e4cb2351fd9
jam0928 <jack020928@naver.com>
2024-11-28 19:19:18 +0900

    Chore: Github 소셜 로그인 구현

--- src/auth.config.js
+++ src/auth.config.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GithubStrategy } from "passport-github2";
 import { prisma } from "./db.config.js";
 
 dotenv.config();
@@ -35,11 +36,54 @@ const googleVerify = async (profile) => {
       data: {
         email,
         name: profile.displayName,
-        gender: "추후 수정",
+        gender: "non",
         birth: new Date(1970, 0, 1),
-        address: "추후 수정",
-        detailAddress: "추후 수정",
-        phoneNumber: "추후 수정",
+        address: "non",
+        detailAddress: "non",
+        phoneNumber: "non",
+      },
+    });
+  
+    return { id: created.id, email: created.email, name: created.name };
+  };
+
+
+  //githubVeirify 함수를 만들어서 사용자 정보를 저장하거나 로직을 처리!!!!!!!!!!!!!!!!!
+  export const githubStrategy = new GithubStrategy(
+    {
+      clientID: process.env.PASSPORT_GITHUB_CLIENT_ID,
+      clientSecret: process.env.PASSPORT_GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/oauth2/callback/github",
+      scope: ["email", "profile"],
+      state: true,
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      return githubVeirify(profile)
+        .then((user) => cb(null, user))
+        .catch((err) => cb(err));
+    }
+  );
+
+  const githubVeirify = async (profile) => {  
+    const email = profile.emails?.[0]?.value;
+    if (!email) {
+      throw new Error(`Github email is not public or accessible: ${JSON.stringify(profile)}`);
+    }
+  
+    const user = await prisma.user.findFirst({ where: { email } });
+    if (user !== null) {
+      return { id: user.id, email: user.email, name: user.name };
+    }
+  
+    const created = await prisma.user.create({
+      data: {
+        email,
+        name: profile.displayName,
+        gender: "non",
+        birth: new Date(1970, 0, 1),
+        address: "non",
+        detailAddress: "non",
+        phoneNumber: "non",
       },
     });
   
